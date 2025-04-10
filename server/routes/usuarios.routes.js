9bccafe0f1793b70534d1a9540ef471a3d756a20
DiegoCass01 <diego.castillo170901@gmail.com>
2025-04-09 23:28:39 -0600

    fix: Correct SQL table name in user routes and remove unused field from user creation

--- server/routes/usuarios.routes.js
+++ server/routes/usuarios.routes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 // Obtener todos los usuarios
 router.get("/", verifyToken, verifyRole("admin"), (req, res) => {
-  pool.query("SELECT * FROM usuarios", (err, results) => {
+  pool.query("SELECT * FROM usuario", (err, results) => {
     if (err) {
       console.error("Error al obtener usuarios:", err);
       res.status(500).json({ error: "Error al obtener los datos" });
@@ -19,13 +19,12 @@ router.get("/", verifyToken, verifyRole("admin"), (req, res) => {
 
 // Crear un nuevo usuario
 router.post("/", verifyToken, verifyRole("admin"), (req, res) => {
-  const { nombre, a_paterno, a_materno, n_usuario, correo, password, rol } =
-    req.body;
+  const { nombre, a_paterno, a_materno, n_usuario, password, rol } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
-  const query = `INSERT INTO usuarios ( nombre, a_paterno, a_materno,n_usuario, correo, password, rol) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+  const query = `INSERT INTO usuario ( nombre, a_paterno, a_materno,n_usuario, password, rol) VALUES (?, ?, ?, ?, ?, ?, ?)`;
   pool.query(
     query,
-    [nombre, a_paterno, a_materno, n_usuario, correo, hashedPassword, rol],
+    [nombre, a_paterno, a_materno, n_usuario, hashedPassword, rol],
     (err) => {
       if (err) return res.status(500).json({ error: err });
       res.status(201).json({ message: "Usuario registrado" });
