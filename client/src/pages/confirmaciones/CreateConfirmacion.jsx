d8174bfa8407671f39429cd40b6b0b51ec139f50
DiegoCass01 <diego.castillo170901@gmail.com>
2025-04-11 00:55:51 -0600

    style: Refactor CreateConfirmacion form structure for improved readability and organization style: Update SearchSacramento styles for better layout and typography consistency

--- client/src/pages/confirmaciones/CreateConfirmacion.jsx
+++ client/src/pages/confirmaciones/CreateConfirmacion.jsx
@@ -64,42 +64,44 @@ export default function CreateConfirmacion({ showSnackbar }) {
   return (
     <div className="form-div">
       <h1>Registro de Confirmaciones</h1>
-      <form onSubmit={handleSubmit} className="form-container">
-        {/* DATOS DEL CONFIRMADO */}
-        <fieldset>
-          <legend>Datos del Confirmado</legend>
-          <FormGroup id="nombre" label="Nombre" value={confirmacion.nombre} onChange={handleChange} required />
-          <FormGroup id="a_paterno" label="Apellido Paterno" value={confirmacion.a_paterno} onChange={handleChange} required />
-          <FormGroup id="a_materno" label="Apellido Materno" value={confirmacion.a_materno} onChange={handleChange} required />
-        </fieldset>
-        <br />
-        {/* DATOS DEL PADRE */}
-        <fieldset>
-          <legend>Datos del Padre</legend>
-          <FormGroup id="nom_padre" label="Nombre del Padre" value={confirmacion.nom_padre} onChange={handleChange} required />
-          <FormGroup id="a_pat_padre" label="Apellido Paterno del Padre" value={confirmacion.a_pat_padre} onChange={handleChange} required />
-          <FormGroup id="a_mat_padre" label="Apellido Materno del Padre" value={confirmacion.a_mat_padre} onChange={handleChange} required />
-        </fieldset>
-        <br />
-        {/* DATOS DE LA MADRE */}
-        <fieldset>
-          <legend>Datos de la Madre</legend>
-          <FormGroup id="nom_madre" label="Nombre de la Madre" value={confirmacion.nom_madre} onChange={handleChange} required />
-          <FormGroup id="a_pat_madre" label="Apellido Paterno de la Madre" value={confirmacion.a_pat_madre} onChange={handleChange} required />
-          <FormGroup id="a_mat_madre" label="Apellido Materno de la Madre" value={confirmacion.a_mat_madre} onChange={handleChange} required />
-        </fieldset>
-        <br />
-        {/* DATOS DEL SACRAMENTO */}
-        <fieldset>
-          <legend>Datos de la Confirmación</legend>
-          <FormGroup id="fecha_confirmacion" label="Fecha de Confirmación" value={confirmacion.fecha_confirmacion} onChange={handleChange} type="date" required />
-          <FormGroup id="libro" label="Libro" value={confirmacion.libro} onChange={handleChange} required />
-          <FormGroup id="foja" label="Foja" value={confirmacion.foja} onChange={handleChange} required />
-          <FormGroup id="acta" label="Acta" value={confirmacion.acta} onChange={handleChange} required />
-        </fieldset>
+      <section className="form-container">
+        <form onSubmit={handleSubmit}>
+          {/* DATOS DEL CONFIRMADO */}
+          <fieldset>
+            <legend>Datos del Confirmado</legend>
+            <FormGroup id="nombre" label="Nombre" value={confirmacion.nombre} onChange={handleChange} required />
+            <FormGroup id="a_paterno" label="Apellido Paterno" value={confirmacion.a_paterno} onChange={handleChange} required />
+            <FormGroup id="a_materno" label="Apellido Materno" value={confirmacion.a_materno} onChange={handleChange} required />
+          </fieldset>
+          <br />
+          {/* DATOS DEL PADRE */}
+          <fieldset>
+            <legend>Datos del Padre</legend>
+            <FormGroup id="nom_padre" label="Nombre del Padre" value={confirmacion.nom_padre} onChange={handleChange} required />
+            <FormGroup id="a_pat_padre" label="Apellido Paterno del Padre" value={confirmacion.a_pat_padre} onChange={handleChange} required />
+            <FormGroup id="a_mat_padre" label="Apellido Materno del Padre" value={confirmacion.a_mat_padre} onChange={handleChange} required />
+          </fieldset>
+          <br />
+          {/* DATOS DE LA MADRE */}
+          <fieldset>
+            <legend>Datos de la Madre</legend>
+            <FormGroup id="nom_madre" label="Nombre de la Madre" value={confirmacion.nom_madre} onChange={handleChange} required />
+            <FormGroup id="a_pat_madre" label="Apellido Paterno de la Madre" value={confirmacion.a_pat_madre} onChange={handleChange} required />
+            <FormGroup id="a_mat_madre" label="Apellido Materno de la Madre" value={confirmacion.a_mat_madre} onChange={handleChange} required />
+          </fieldset>
+          <br />
+          {/* DATOS DEL SACRAMENTO */}
+          <fieldset>
+            <legend>Datos de la Confirmación</legend>
+            <FormGroup id="fecha_confirmacion" label="Fecha de Confirmación" value={confirmacion.fecha_confirmacion} onChange={handleChange} type="date" required />
+            <FormGroup id="libro" label="Libro" value={confirmacion.libro} onChange={handleChange} required />
+            <FormGroup id="foja" label="Foja" value={confirmacion.foja} onChange={handleChange} required />
+            <FormGroup id="acta" label="Acta" value={confirmacion.acta} onChange={handleChange} required />
+          </fieldset>
 
-        <button type="submit" className="submit-button">Agregar</button>
-      </form>
+          <button type="submit" className="submit-button">Agregar</button>
+        </form>
+      </section>
     </div>
   );
 }
